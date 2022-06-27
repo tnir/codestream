5bf034a6cbca1cfa5bf8d04f602ff88b57308417
Brian Canzanella <brian@codestream.com>
2022-06-27 13:27:00 -0400

    remove Sentry from JB (#1026)

--- jb/src/main/kotlin/telemetry/ErrorHandler.kt
+++ jb/src/main/kotlin/telemetry/ErrorHandler.kt
@@ -9,10 +9,6 @@ import com.intellij.openapi.diagnostic.ErrorReportSubmitter
 import com.intellij.openapi.diagnostic.IdeaLoggingEvent
 import com.intellij.openapi.diagnostic.SubmittedReportInfo
 import com.intellij.util.Consumer
-import io.sentry.Sentry
-import io.sentry.connection.EventSendCallback
-import io.sentry.event.Event
-import io.sentry.event.UserBuilder
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import java.awt.Component
@@ -23,38 +19,6 @@ class ErrorHandler : ErrorReportSubmitter() {
         var userLoggedIn: UserLoggedIn? = null
         var agentService: AgentService? = null
         private var _consumer: Consumer<in SubmittedReportInfo>? = null
-        private var _environment: String = "prod"
-        var environment: String
-            get() = _environment
-            set(value) {
-                _environment = value
-                initSentry()
-            }
-
-        private fun initSentry() {
-            synchronized(ErrorHandler.javaClass) {
-                Sentry.init("https://7c34949981cc45848fc4e3548363bb17@sentry.io/1314159?environment=$environment&uncaught.handler.enabled=false")
-                Sentry.getContext().addTag("platform", platform.name)
-                Sentry.getContext().addTag("ide", Ide.name)
-                Sentry.getContext().addTag("ideVersion", Ide.version)
-                Sentry.getContext().addTag("ideDetail", Ide.detail)
-                Sentry.getContext().addTag("source", "extension")
-
-                Sentry.getStoredClient().addEventSendCallback(object : EventSendCallback {
-                    override fun onSuccess(event: Event?) {
-                        _consumer?.consume(SubmittedReportInfo(SubmittedReportInfo.SubmissionStatus.NEW_ISSUE))
-                    }
-
-                    override fun onFailure(event: Event?, exception: Exception?) {
-                        _consumer?.consume(SubmittedReportInfo(SubmittedReportInfo.SubmissionStatus.FAILED))
-                    }
-                })
-            }
-        }
-    }
-
-    init {
-        initSentry()
     }
 
     override fun getReportActionText(): String {
@@ -69,18 +33,9 @@ class ErrorHandler : ErrorReportSubmitter() {
     ): Boolean {
         _consumer = consumer
 
-        userLoggedIn?.let {
-            Sentry.getContext().user = UserBuilder()
-                .setId(it.userId)
-                .setUsername(it.user.username)
-                .setEmail(it.user.email)
-                .build()
-        }
-
         for (event in events) {
             val logMessage = event.data as? LogMessage
             logMessage?.let {
-                Sentry.capture(it.throwable)
                 GlobalScope.launch {
                     agentService?.reportMessage(it.throwable)
                 }
