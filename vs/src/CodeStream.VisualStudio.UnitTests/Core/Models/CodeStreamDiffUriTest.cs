84239097ef76e888a5da24bcfe6014eed2a988d4
Calvin A. Allen <calvin.allen@outlook.com>
2022-09-28 13:42:29 -0400

    Fixing some tests around temp files and PRs

--- vs/src/CodeStream.VisualStudio.UnitTests/Core/Models/CodeStreamDiffUriTest.cs
+++ vs/src/CodeStream.VisualStudio.UnitTests/Core/Models/CodeStreamDiffUriTest.cs
@@ -1,4 +1,6 @@
-﻿using CodeStream.VisualStudio.Core.Models;
+﻿using System;
+
+using CodeStream.VisualStudio.Core.Models;
 
 using Xunit;
 
@@ -6,33 +8,13 @@ namespace CodeStream.VisualStudio.UnitTests.Core.Models {
 	
 	public class CodeStreamDiffUriTest {
 		[Theory]
-		[InlineData(@"c:/users/foo/code/codestream/codestream-diff/2asdf/1/repoId/left/codestream-diff/foo/bar/baz1.cs", true)]
-		[InlineData(@"c:\users\foo\code\codestream\codestream-diff\2asdf\1\repoId\left\codestream-diff\foo\bar\baz1.cs", true)]
-		[InlineData(@"codestream-diff/2asdf/1/repoId/left/codestream-diff/foo/bar/baz1.cs", true)]
-		[InlineData(@"codestream-diff/1/repoId/left/codestream-diff/foo/bar/baz1.cs", false)]
-		[InlineData(@"codestream-diff://2asdf/1/repoId/left/codestream-diff/foo/bar/baz1.cs", false)]
+		[InlineData(@"c:/users/{0}/AppData/Local/Temp/baz1.cs", true)]
+		[InlineData(@"c:\users\{0}\AppData\Local\Temp\baz1.cs", true)]
 		[InlineData("", false)]
-		[InlineData(null, false)]
-		public void IsTempFileTest(string filePathPart, bool expected) {			
-			Assert.Equal(expected, CodeStreamDiffUri.IsTempFile(filePathPart));
-		}
-		
-		[Theory]
-		[InlineData(@"codestream-diff/reviewId/1/repoId/left/codestream-diff/foo/bar/baz1.cs", true)]
-		[InlineData(@"codestream-diff/reviewId/undefined/repoId/left/codestream-diff/foo/bar/baz1.cs", true)]
-		[InlineData(@"codestream-diff/1/repoId/left/codestream-diff/foo/bar/baz1.cs", false)]
-		[InlineData(@"codestream-diff://2asdf/1/repoId/left/codestream-diff/foo/bar/baz1.cs", false)]
-		[InlineData("", false)]
-		[InlineData(null, false)]
-		public void TryParseTest(string filePathPart, bool expected) {
-			Assert.Equal(expected, CodeStreamDiffUri.TryParse(filePathPart, out CodeStreamDiffUri result));
-		}
-		
-		[Theory]
-		[InlineData(@"codestream-diff/reviewId/undefined/repoId/left/codestream-diff/foo/bar/baz1.cs", "foo/bar/baz1.cs", true)]
-		public void TryParseFileNameTest(string filePathPart, string fileName, bool expected) {
-			Assert.Equal(expected, CodeStreamDiffUri.TryParse(filePathPart, out CodeStreamDiffUri result));
-			Assert.Equal(fileName, result.FileName);
+		public void IsTempFileTest(string filePathPart, bool expected)
+		{
+			var filePath = string.Format(filePathPart, Environment.UserName);
+			Assert.Equal(expected, DiffExtensions.IsTempFile(filePath));
 		}
 	}
 }
