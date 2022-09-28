3f750e32a6df980f70cb6077103fad56f509eaab
Calvin A. Allen <calvin.allen@outlook.com>
2022-09-28 13:53:26 -0400

    Since the username is part of the path, and the CI server runs these a bit differently as "SYSTEM", this test has to be reworked a bit

---
+++ vs/src/CodeStream.VisualStudio.UnitTests/Core/Models/DiffExtensionTests.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+using CodeStream.VisualStudio.Core.Models;
+
+using Xunit;
+
+namespace CodeStream.VisualStudio.UnitTests.Core.Models {
+	
+	public class DiffExtensionTests {
+		[Fact]
+		public void IsTempFileTest()
+		{
+			var tempFile = Path.GetTempFileName();
+			Assert.True(DiffExtensions.IsTempFile(tempFile));
+		}
+	}
+}
