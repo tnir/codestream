5e75d0b4bb1b7cbf8883203abdf3785d93ba73ed
Calvin A. Allen <calvinallen@newrelic.com>
2022-10-21 16:32:08 -0400

    Update build scripts for VS to only pull token file when absolutely necessary, and clean it up after its no longer needed (#1227)

--- vs/build/pre-build.ps1
+++ vs/build/pre-build.ps1
@@ -2,14 +2,13 @@ param([string] $checkoutDir = $pwd, [string] $assetEnv = "")
 
 $computer = 'teamcity.codestream.us'
 $username = 'web'
-$homeDir = 'C:\Users\Administrator'
-$keyfile = $homeDir + '\.ssh\id_rsa'
+$keyfile = 'C:\Users\Administrator\.ssh\id_rsa'
 $localReleaseLicenseFile = $checkoutDir + '\vs\licenses\Release\teamdev.licenses'
 $localDebugLicenseFile = $checkoutDir + '\vs\licenses\Debug\teamdev.licenses'
 $remoteLicenseFile = '/home/web/.codestream/licenses/teamdev/DotNetBrowser/runtime/teamdev.licenses'
-$localVSCETokenFile = $homeDir + '\.vsce'
-$remoteVSCETokenFile = '/home/web/.codestream/microsoft/vsce-credentials'
 
+Write-Host ""
+Write-Host "---- Script Values ----"
 Write-Host 'Checkout Dir  : ' $checkoutDir
 Write-Host 'PSScriptRoot  : ' $PSScriptRoot
 Write-Host 'Build Number  : ' $env:BUILD_NUMBER
@@ -17,15 +16,14 @@ Write-Host 'Build Counter : ' $env:TCBUILD_COUNTER
 Write-Host 'Asset-Env     : ' $assetEnv
 Write-Host 'localicenseFile: ' $localReleaseLicenseFile
 Write-Host 'remoteLicenseFile: ' $remoteLicenseFile
+Write-Host "-----------------------"
+Write-Host ""
 
 $cred = new-object -typename System.Management.Automation.PSCredential $username, (new-object System.Security.SecureString)
 
 # Get the teamdev license
 Get-SCPFile -ComputerName $computer -LocalFile $localReleaseLicenseFile -RemoteFile $remoteLicenseFile -KeyFile $keyfile -Credential $cred -AcceptKey
 Get-SCPFile -ComputerName $computer -LocalFile $localDebugLicenseFile -RemoteFile $remoteLicenseFile -KeyFile $keyfile -Credential $cred -AcceptKey
-
-# Get the VSCE Marketplace Token File
-Get-SCPFile -ComputerName $computer -LocalFile $localVSCETokenFile -RemoteFile $remoteVSCETokenFile -KeyFile $keyfile -Credential $cred -AcceptKey
 
 if (!(Test-Path -Path $localReleaseLicenseFile)) {
     Write-Host "localReleaseLicenseFile not found ($localReleaseLicenseFile)"
--- vs/build/publish-to-marketplace.ps1
+++ vs/build/publish-to-marketplace.ps1
@@ -1,10 +1,25 @@
 [CmdletBinding(SupportsShouldProcess)]
 param([string] $checkoutDir = $pwd, [string] $assetEnv = "", [string] $buildNumber = $env:build_number)
 
-$homeDir = 'C:\Users\Administrator'
-$localVSCETokenFile = $homeDir + '\.vsce'
-
 Write-Host '**** The script is running in directory' (Get-Location)
+
+$username = 'web'
+$computer = 'teamcity.codestream.us'
+$keyfile = 'C:\Users\Administrator\.ssh\id_rsa'
+$localVSCETokenFile = "$env:TEMP\codestream.vsce"
+$remoteVSCETokenFile = '/home/web/.codestream/microsoft/vsce-credentials'
+
+Write-Host ""
+Write-Host "---- Script Values ----"
+Write-Host 'PSScriptRoot  : ' $PSScriptRoot
+Write-Host 'localVSCETokenFile: ' $localVSCETokenFile
+Write-Host "-----------------------"
+Write-Host ""
+
+$cred = new-object -typename System.Management.Automation.PSCredential $username, (new-object System.Security.SecureString)
+Get-SCPFile -ComputerName $computer -LocalFile $localVSCETokenFile -RemoteFile $remoteVSCETokenFile -KeyFile $keyfile -Credential $cred -AcceptKey
+
+$exception = $False
 $vsDir = $checkoutDir + '\vs'
 $buildDir = $vsDir + '\build'
 $x86AssetDir = $buildDir + '\artifacts\Release\x86'
@@ -16,17 +31,20 @@ $x64Asset = $x64AssetDir + '\codestream-vs-' + $buildNumber + '-x64.vsix'
 Write-Host 'Here is the x86 VSIX file (' $x86Asset '):'
 Get-ChildItem $x86Asset
 if ($LastExitCode -ne $null -and $LastExitCode -ne 0) {
+    deleteTokenFile($localVSCETokenFile)
     exit 1
 }
 
 Write-Host 'Here is the x64 VSIX file (' $x64Asset '):'
 Get-ChildItem $x64Asset
 if ($LastExitCode -ne $null -and $LastExitCode -ne 0) {
+    deleteTokenFile($localVSCETokenFile)
     exit 1
 }
 
 $pat = (Get-Content -Raw -Path $localVSCETokenFile | ConvertFrom-Json).publishers.Where({$_.Name -eq "CodeStream"}).pat
 if ($LastExitCode -ne $null -and $LastExitCode -ne 0) {
+    deleteTokenFile($localVSCETokenFile)
     exit 1
 }
 Write-Host "Got PAT Length=$($pat.Length)"
@@ -44,15 +62,33 @@ else {
     Write-Host 'Publishing assets to marketplace...'
     # https://docs.microsoft.com/en-us/visualstudio/extensibility/walkthrough-publishing-a-visual-studio-extension-via-command-line?view=vs-2019
     #  -ignoreWarnings "VSIXValidatorWarning01,VSIXValidatorWarning02"
-    & $exe publish -payload $x86Asset -publishManifest "$($vsDir)\src\CodeStream.VisualStudio.Vsix.x86\publish\publishManifest.json" -personalAccessToken $pat
-    if ($LastExitCode -ne $null -and $LastExitCode -ne 0) {
-        Write-Error "Failed to publish $($x86Asset) to marketplace"
-		exit 1
-	}
+    try{
+        & $exe publish -payload $x86Asset -publishManifest "$($vsDir)\src\CodeStream.VisualStudio.Vsix.x86\publish\publishManifest.json" -personalAccessToken $pat
+        if ($LastExitCode -ne $null -and $LastExitCode -ne 0) {
+            throw "Failed to publish $($x86Asset) to marketplace"
+        }
+    
+        & $exe publish -payload $x64Asset -publishManifest "$($vsDir)\src\CodeStream.VisualStudio.Vsix.x64\publish\publishManifest.json" -personalAccessToken $pat
+        if ($LastExitCode -ne $null -and $LastExitCode -ne 0) {
+            throw "Failed to publish $($x64Asset) to marketplace"
+        }
+    }
+    catch{
+        Write-Error "$_"
+        $exception = $True
+    }
+    finally{
+        # Clean up the token file so it doesn't hang around longer than necessary
+        deleteTokenFile($localVSCETokenFile)
+    }
 
-    & $exe publish -payload $x64Asset -publishManifest "$($vsDir)\src\CodeStream.VisualStudio.Vsix.x64\publish\publishManifest.json" -personalAccessToken $pat
-    if ($LastExitCode -ne $null -and $LastExitCode -ne 0) {
-        Write-Error "Failed to publish $($x64Asset) to marketplace"
-		exit 1
-	}
+    if($exception -eq $True){
+        exit 1
+    }
+}
+
+function deleteTokenFile($tokenPath){
+    if(Test-Path $tokenPath){
+        Remove-Item $tokenPath
+    }
 }
