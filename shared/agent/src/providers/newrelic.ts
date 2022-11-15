e3a94e9a5ea7f6447194b458da1d7224f8f55b92
Brian Canzanella <brian@codestream.com>
2022-11-15 15:57:43 -0500

    guard against missing timestamps in NR queries

--- shared/agent/src/providers/newrelic.ts
+++ shared/agent/src/providers/newrelic.ts
@@ -1189,18 +1189,16 @@ export class NewRelicProvider extends ThirdPartyIssueProviderBase<CSNewRelicProv
 
 				if (errorGroupResponse.eventsQuery) {
 					const timestampRange = this.generateTimestampRange(request.timestamp);
-					const nrql =
-						errorGroupResponse.eventsQuery +
-						`since ${timestampRange?.startTime}` +
-						`until ${timestampRange?.endTime}` +
-						"LIMIT 1";
-					const result = await this.runNrql<{
-						"tags.releaseTag": string;
-						"tags.commit": string;
-					}>(accountId, nrql);
-					if (result.length) {
-						errorGroup.releaseTag = result[0]["tags.releaseTag"];
-						errorGroup.commit = result[0]["tags.commit"];
+					if (timestampRange) {
+						const nrql = `${errorGroupResponse.eventsQuery} since ${timestampRange?.startTime} until ${timestampRange?.endTime} LIMIT 1`;
+						const result = await this.runNrql<{
+							"tags.releaseTag": string;
+							"tags.commit": string;
+						}>(accountId, nrql);
+						if (result.length) {
+							errorGroup.releaseTag = result[0]["tags.releaseTag"];
+							errorGroup.commit = result[0]["tags.commit"];
+						}
 					}
 				}
 
