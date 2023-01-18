a39e85fc7c0c1babf4b0b4cffffc3097c04eb73c
Renee Petit <renee.petit@gmail.com>
2023-01-18 07:38:43 -0800

    Removes option for expand/collapse & removes chevron (#1310)
    * file w/ comments always expanded
    
    * comment out unnecessary code
    
    * delete unnecessary code
    
    Co-authored-by: Renee Petit <rpetit@newrelic.com>

--- shared/ui/Stream/PullRequestFilesChangedFileComments.tsx
+++ shared/ui/Stream/PullRequestFilesChangedFileComments.tsx
@@ -204,11 +204,6 @@ export const PullRequestFilesChangedFileComments = (props: Props) => {
 		}
 	};
 
-	const handleClick = e => {
-		e.preventDefault();
-		setShowComments(!showComments);
-	};
-
 	/**
 	 * Github/lab makes it difficult to find a comment line number, so we have to
 	 * parse the diffHunk and do some basic math
@@ -420,11 +415,11 @@ export const PullRequestFilesChangedFileComments = (props: Props) => {
 		// hasComments
 		return (
 			<div onMouseEnter={e => handleMouseEnter(e)} onMouseLeave={e => handleMouseLeave(e)}>
-				<FileWithComments onClick={e => handleClick(e)}>
+				<FileWithComments>
 					<ChangesetFile
-						chevron={<Icon name={showComments ? "chevron-down-thin" : "chevron-right-thin"} />}
 						selected={selected}
 						viewMode={props.viewMode}
+						// This is for the additions & deletions count
 						count={
 							<div style={{ margin: "0 10px 0 auto", display: "flex" }}>
 								{comments.length === 0 || showComments ? null : (
@@ -441,6 +436,7 @@ export const PullRequestFilesChangedFileComments = (props: Props) => {
 									display: showGoToFileIcon ? "flex" : "none",
 								}}
 							>
+								{/* this is for the open local file icon on the right hand side */}
 								<Icon
 									title="Open Local File"
 									placement="bottom"
@@ -455,6 +451,7 @@ export const PullRequestFilesChangedFileComments = (props: Props) => {
 						iconLast={
 							isDisabled ? null : (
 								<>
+									{/* This is for the mark as viewed icon on the right hand side */}
 									{iconIsFlex && (
 										<span
 											style={{
@@ -505,38 +502,33 @@ export const PullRequestFilesChangedFileComments = (props: Props) => {
 						customFilenameColor={"var(--text-color-filename-highlight)"}
 					/>
 				</FileWithComments>
-				{showComments && (
-					<>
-						{commentsSortedByLineNumber.map((c, index) => {
-							const isPending = c.comment.state === "PENDING";
-							return (
-								<Comment
-									onClick={e => handleCommentClick(e, c)}
-									style={depth ? { paddingLeft: `${depth * 12}px` } : {}}
-									key={`comment_${c.comment.id}_${index}`}
+				{/* showComments */}
+				{commentsSortedByLineNumber.map((c, index) => {
+					const isPending = c.comment.state === "PENDING";
+					return (
+						<Comment
+							onClick={e => handleCommentClick(e, c)}
+							style={depth ? { paddingLeft: `${depth * 12}px` } : {}}
+							key={`comment_${c.comment.id}_${index}`}
+						>
+							<div style={{ display: "flex" }}>
+								<div
+									style={{
+										overflow: "hidden",
+										textOverflow: "ellipsis",
+										width: "calc(100%)",
+										whiteSpace: "nowrap",
+									}}
 								>
-									<div style={{ display: "flex" }}>
-										<div
-											style={{
-												overflow: "hidden",
-												textOverflow: "ellipsis",
-												width: "calc(100%)",
-												whiteSpace: "nowrap",
-											}}
-										>
-											<Icon name="comment" className="type-icon" />{" "}
-											{lineNumber(c) && <span>Line {lineNumber(c)}: </span>}
-											{c.comment.bodyText || c.comment.body || c.comment.content.raw}
-										</div>
-										{isPending && (
-											<PendingCircle onClick={e => handlePendingClick(e)}>P</PendingCircle>
-										)}
-									</div>
-								</Comment>
-							);
-						})}
-					</>
-				)}
+									<Icon name="comment" className="type-icon" />{" "}
+									{lineNumber(c) && <span>Line {lineNumber(c)}: </span>}
+									{c.comment.bodyText || c.comment.body || c.comment.content.raw}
+								</div>
+								{isPending && <PendingCircle onClick={e => handlePendingClick(e)}>P</PendingCircle>}
+							</div>
+						</Comment>
+					);
+				})}
 			</div>
 		);
 	}
