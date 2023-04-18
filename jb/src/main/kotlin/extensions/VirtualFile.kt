67e77d78720bbfa650065b0c6371fb053d26841c
Dillon Sellars <dsellars@newrelic.com>
2023-04-18 14:52:47 -0400

    NR-105605 Error on WebStorm IDE GH #1420

--- jb/src/main/kotlin/extensions/VirtualFile.kt
+++ jb/src/main/kotlin/extensions/VirtualFile.kt
@@ -3,21 +3,29 @@ package com.codestream.extensions
 import com.codestream.system.HASH_ENCODED
 import com.codestream.system.SPACE_ENCODED
 import com.codestream.system.sanitizeURI
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.vfs.VirtualFile
 import java.net.URL
 
+private val logger = Logger.getInstance(VirtualFile::class.java)
+
 val VirtualFile.uri: String?
     get() {
+        val isCsDiff = url.startsWith("codestream-diff://")
+        val workingUrl = if (isCsDiff) url.replace(Regex("""^codestream-diff://"""), "file://") else url
         return try {
-            sanitizeURI(
-                URL(
-                    url
-                        .replace(" ", SPACE_ENCODED)
-                        .replace("#", HASH_ENCODED)
-                ).toURI().toString()
-            )
+            val uriStrEncoded = workingUrl
+                .replace(" ", SPACE_ENCODED)
+                .replace("#", HASH_ENCODED)
+            val uriStr = try {
+                URL(uriStrEncoded).toURI().toString()
+            } catch (e: Exception) {
+                uriStrEncoded
+            }
+            val finalUrl = if (isCsDiff) uriStr.replace(Regex("""^file://"""), "codestream-diff://") else uriStr
+            sanitizeURI(finalUrl)
         } catch (e: Exception) {
-            // LOG.warn(e)
+            logger.warn("Error in VirtualFile.uri", e)
             null
         }
     }
