6bf0ad6470440abb6b6c90ff22a4218a349bc254
Eric Jones <etjones319@gmail.com>
2023-05-11 11:15:18 -0700

    URL migration additions for VSC

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -54,7 +54,9 @@ interface BuildInfoMetadata {
 export const IDE_NAME = "VS Code";
 
 const serverUrlMigrations: { [url: string]: string } = {
-	"https://staging-api.codestream.us": "https://codestream-stg.staging-service.newrelic.com"
+	"https://staging-api.codestream.us": "https://codestream-stg.staging-service.newrelic.com",
+	"https://api.codestream.com//": "https://codestream-us1.service.newrelic.com",
+	"https://eu-api.codestream.com": "https://codestream-eu1.service.eu.newrelic.com"
 };
 
 export async function activate(context: ExtensionContext) {
