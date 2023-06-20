d9e8044a3a7b0b9e377370f723fecd68b4a8807e
Angie Yeh <ayeh@newrelic.com>
2023-06-20 10:49:08 -0700

    Add Fossa button into the integration menu

--- shared/ui/Stream/IntegrationsPanel.tsx
+++ shared/ui/Stream/IntegrationsPanel.tsx
@@ -112,6 +112,10 @@ export const IntegrationsPanel = () => {
 			.filter(id => providers[id].hasSharing)
 			.filter(id => !teamSettings.limitMessaging || teamMessagingProviders[id])
 			.sort((a, b) => providers[a].name.localeCompare(providers[b].name));
+		const codeAnalyzersProviders = Object.keys(providers)
+			.filter(id => ["fossa"].includes(providers[id].name))
+			.filter(id => !connectedProviders.includes(id))
+			.sort((a, b) => providers[a].name.localeCompare(providers[b].name));
 		const cicdProviders = Object.keys(providers)
 			.filter(id => ["circleci"].includes(providers[id].name))
 			.filter(id => !connectedProviders.includes(id))
@@ -125,6 +129,7 @@ export const IntegrationsPanel = () => {
 			observabilityProviders,
 			issueProviders,
 			messagingProviders,
+			codeAnalyzersProviders,
 			cicdProviders,
 			connectedProviders,
 			sharingTargets,
@@ -207,9 +212,11 @@ export const IntegrationsPanel = () => {
 			if (!display) return null;
 
 			const displayHost = renderDisplayHost(host);
-			const displayName = isEnterprise
+			let displayName = isEnterprise
 				? `${display.displayName} - ${displayHost}`
 				: display.displayName;
+			displayName = display.hideDisplayName ? "" : display.displayName;
+
 			const action = () => dispatch(configureAndConnectProvider(providerId, "Integrations Panel"));
 			/*
 
@@ -352,6 +359,11 @@ export const IntegrationsPanel = () => {
 
 						<h2>Issue Providers</h2>
 						<IntegrationButtons>{renderProviders(derivedState.issueProviders)}</IntegrationButtons>
+
+						<h2>Code Analyzers</h2>
+						<IntegrationButtons>
+							{renderProviders(derivedState.codeAnalyzersProviders)}
+						</IntegrationButtons>
 
 						{derivedState.cicdProviders.length > 0 && (
 							<>
