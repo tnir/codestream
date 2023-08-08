c5d0b51fd5d99e701d2373bd3ac519bd24c99d28
Calvin Allen <calvinallen@newrelic.com>
2023-08-08 17:37:11 -0400

    Fix the expressions

--- vs/build/publish-to-marketplace.ps1
+++ vs/build/publish-to-marketplace.ps1
@@ -31,7 +31,7 @@ $x64Asset = $x64AssetDir + '\codestream-vs-' + $buildNumber + '-x64.vsix'
 Write-Host 'Here is the x86 VSIX file (' $x86Asset '):'
 Get-ChildItem $x86Asset
 if ($LastExitCode -ne $null -and $LastExitCode -ne 0) {
-    if(Test-Path $tokenPath){
+    if((Test-Path -Path $tokenPath) -eq $True){
         Remove-Item $tokenPath
     }
     exit 1
@@ -40,7 +40,7 @@ if ($LastExitCode -ne $null -and $LastExitCode -ne 0) {
 Write-Host 'Here is the x64 VSIX file (' $x64Asset '):'
 Get-ChildItem $x64Asset
 if ($LastExitCode -ne $null -and $LastExitCode -ne 0) {
-    if(Test-Path $tokenPath){
+    if((Test-Path -Path $tokenPath) -eq $True){
         Remove-Item $tokenPath
     }
     exit 1
@@ -48,7 +48,7 @@ if ($LastExitCode -ne $null -and $LastExitCode -ne 0) {
 
 $pat = (Get-Content -Raw -Path $localVSCETokenFile | ConvertFrom-Json).publishers.Where({$_.Name -eq "CodeStream"}).pat
 if ($LastExitCode -ne $null -and $LastExitCode -ne 0) {
-    if(Test-Path $tokenPath){
+    if((Test-Path -Path $tokenPath) -eq $True){
         Remove-Item $tokenPath
     }
     exit 1
@@ -86,7 +86,7 @@ else {
     }
     finally{
         # Clean up the token file so it doesn't hang around longer than necessary
-        if(Test-Path $tokenPath){
+        if((Test-Path -Path $tokenPath) -eq $True){
             Remove-Item $tokenPath
         }
     }
