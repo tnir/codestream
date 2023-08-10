6a29e180462952bbe57973b4cbb6500b311c81b2
Angie Yeh <ayeh@newrelic.com>
2023-08-10 13:58:02 -0700

    Make skeleton loader for loading pagination results

--- shared/ui/Stream/CodeAnalyzers/FossaLoading.tsx
+++ shared/ui/Stream/CodeAnalyzers/FossaLoading.tsx
@@ -4,30 +4,44 @@ import { SkeletonLoader, SkeletonFlexContainer } from "@codestream/webview/Strea
 export const FossaLoading = () => {
 	return (
 		<>
+			{/* Vulnerability section */}
 			<SkeletonLoader style={{ width: "50%", marginLeft: "20px" }} />
 			<SkeletonFlexContainer>
-				<SkeletonLoader style={{ width: "65%", margin: "0px 10px 3px 40px" }} />
+				<SkeletonLoader style={{ width: "65%", margin: "0px 10px 3px 30px" }} />
 				<SkeletonLoader style={{ width: "6%", margin: "0px 10px 3px 0px" }} />
 			</SkeletonFlexContainer>
 			<SkeletonFlexContainer>
-				<SkeletonLoader style={{ width: "45%", margin: "3px 10px 3px 40px" }} />
+				<SkeletonLoader style={{ width: "45%", margin: "3px 10px 3px 30px" }} />
 				<SkeletonLoader style={{ width: "6%", margin: "3px 10px 3px 0px" }} />
 			</SkeletonFlexContainer>
 			<SkeletonFlexContainer>
-				<SkeletonLoader style={{ width: "45%", margin: "3px 10px 3px 40px" }} />
+				<SkeletonLoader style={{ width: "45%", margin: "3px 10px 3px 30px" }} />
 				<SkeletonLoader style={{ width: "6%", margin: "3px 10px 3px 0px" }} />
 			</SkeletonFlexContainer>
 			<SkeletonFlexContainer>
-				<SkeletonLoader style={{ width: "55%", margin: "3px 10px 3px 40px" }} />
+				<SkeletonLoader style={{ width: "55%", margin: "3px 10px 3px 30px" }} />
 				<SkeletonLoader style={{ width: "6%", margin: "3px 10px 3px 0px" }} />
 			</SkeletonFlexContainer>
-
+			{/* License dependency section */}
 			<SkeletonLoader style={{ width: "45%", marginLeft: "20px" }} />
-			<SkeletonLoader style={{ width: "70%", margin: "3px 10px 3px 40px" }} />
-			<SkeletonLoader style={{ width: "60%", margin: "3px 10px 3px 40px" }} />
-			<SkeletonLoader style={{ width: "50%", margin: "3px 10px 3px 40px" }} />
-			<SkeletonLoader style={{ width: "70%", margin: "3px 10px 3px 40px" }} />
-			<SkeletonLoader style={{ width: "65%", margin: "3px 10px 3px 40px" }} />
+			<SkeletonFlexContainer>
+				<SkeletonLoader style={{ width: "70%", margin: "3px 10px 3px 30px" }} />
+			</SkeletonFlexContainer>
+			<SkeletonFlexContainer>
+				<SkeletonLoader style={{ width: "70%", margin: "3px 10px 3px 30px" }} />
+			</SkeletonFlexContainer>
+			<SkeletonFlexContainer>
+				<SkeletonLoader style={{ width: "60%", margin: "3px 10px 3px 30px" }} />
+			</SkeletonFlexContainer>
+			<SkeletonFlexContainer>
+				<SkeletonLoader style={{ width: "50%", margin: "3px 10px 3px 30px" }} />
+			</SkeletonFlexContainer>
+			<SkeletonFlexContainer>
+				<SkeletonLoader style={{ width: "70%", margin: "3px 10px 3px 30px" }} />
+			</SkeletonFlexContainer>
+			<SkeletonFlexContainer>
+				<SkeletonLoader style={{ width: "65%", margin: "3px 10px 3px 30px" }} />
+			</SkeletonFlexContainer>
 		</>
 	);
 };
@@ -36,11 +50,7 @@ export const IssuesLoading = () => {
 	return (
 		<>
 			<SkeletonFlexContainer>
-				<SkeletonLoader style={{ width: "55%", margin: "3px 10px 3px 40px" }} />
-				<SkeletonLoader style={{ width: "6%", margin: "3px 10px 3px 0px" }} />
-			</SkeletonFlexContainer>
-			<SkeletonFlexContainer>
-				<SkeletonLoader style={{ width: "45%", margin: "3px 10px 3px 40px" }} />
+				<SkeletonLoader style={{ width: "55%", margin: "3px 10px 3px 30px" }} />
 				<SkeletonLoader style={{ width: "6%", margin: "3px 10px 3px 0px" }} />
 			</SkeletonFlexContainer>
 		</>
