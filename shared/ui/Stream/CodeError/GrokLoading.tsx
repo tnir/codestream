8678934106fda556d33ec9bddf631510daf67db7
Eric Jones <etjones319@gmail.com>
2023-08-16 09:54:36 -0700

    adjust grok skele loader styles (#1687)

--- shared/ui/Stream/CodeError/GrokLoading.tsx
+++ shared/ui/Stream/CodeError/GrokLoading.tsx
@@ -4,8 +4,15 @@ import { SkeletonLoader } from "@codestream/webview/Stream/SkeletonLoader";
 export const GrokLoading = () => {
 	return (
 		<>
-			<SkeletonLoader style={{ width: "90%", marginLeft: "25px" }} />
-			<SkeletonLoader style={{ width: "90%", marginLeft: "25px", marginBottom: "5px" }} />
+			<SkeletonLoader
+				style={{ width: "90%", marginLeft: "25px", marginTop: "4px", marginBottom: "4px" }}
+			/>
+			<SkeletonLoader
+				style={{ width: "90%", marginLeft: "25px", marginTop: "4px", marginBottom: "4px" }}
+			/>
+			<SkeletonLoader
+				style={{ width: "50%", marginLeft: "25px", marginTop: "4px", marginBottom: "4px" }}
+			/>
 		</>
 	);
 };
