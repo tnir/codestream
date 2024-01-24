f8214c15fae431d67b07538e171980cd7a955181
Calvin Allen <calvinallen@newrelic.com>
2024-01-24 14:34:42 -0500

    Fix logic

--- shared/ui/utilities/newRelic.ts
+++ shared/ui/utilities/newRelic.ts
@@ -4,12 +4,14 @@ export function parseId(entityGuid: string) {
 		const split = parsed.split(/\|/);
 
 		// "140272|ERT|ERR_GROUP|12076a73-fc88-3205-92d3-b785d12e08b6"
-		const [accountId, unknownAbbreviation, entityType, unknownGuid] = split;
+		const [accountId, entityDomain, entityType, appId] = split;
 
 		return {
-			accountId: !accountId ? parseInt(accountId, 10) : 0,
+			accountId: accountId ? parseInt(accountId, 10) : 0,
 			entityGuid,
 			entityType,
+			entityDomain,
+			appId,
 		};
 	} catch {
 		return undefined;
