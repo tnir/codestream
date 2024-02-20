5c0741a51912f0b7f5c7d430507f81b15c79d27d
Marcelo Bukowski de Farias <mfarias@newrelic.com>
2024-02-20 17:23:08 -0300

    Smart extraction of log search query for JB

--- jb/src/main/kotlin/actions/LogSearch.kt
+++ jb/src/main/kotlin/actions/LogSearch.kt
@@ -1,14 +1,40 @@
 package com.codestream.actions
 
 import com.codestream.editorService
+import com.codestream.extensions.selectionOrCurrentLine
 import com.codestream.protocols.webview.LogsNotifications
 import com.codestream.webViewService
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.util.DocumentUtil
+import java.util.regex.Pattern
 
 class LogSearch: DumbAwareAction() {
     override fun actionPerformed(e: AnActionEvent) {
-        val query = e.project?.editorService?.activeEditor?.selectionModel?.selectedText
+        var query = e.project?.editorService?.activeEditor?.selectionModel?.selectedText ?: ""
+        val editor = e.project?.editorService?.activeEditor
+        val doc = editor?.document
+        val range = editor?.selectionOrCurrentLine
+        if (query.isBlank() && doc != null && range != null) {
+            val lineText = doc.getText(DocumentUtil.getLineTextRange(doc, range.start.line))
+            val pattern = Pattern.compile("\"(?:[^\"]|\"\")*(?:\"|$)|\"(?:[^\"\\\\]|\\\\.)*\"|'(?:[^'\\\\]|\\\\.)*'|`(?:[^'\\\\]|\\\\.)*`", Pattern.MULTILINE or Pattern.CASE_INSENSITIVE)
+            val matches = pattern.matcher(lineText)
+
+            var matchString: String? = null
+            if (matches.find()) {
+                matchString = matches.group(0)
+            }
+
+            if (matchString != null) {
+                query = matchString
+                    .trim()
+                    .replace("(\\$)?\\{.*?}".toRegex(), "") // replace interpolated values - {0}, {variable2}, ${something}, ${variable23}
+                    .replace("^['\"`]".toRegex(), "") // replace leading quote
+                    .replace("['\"`]$".toRegex(), "") // replace trailing quote
+                    .trim()
+
+            }
+        }
         e.project?.webViewService?.postNotification(LogsNotifications.Search(query))
     }
 }
