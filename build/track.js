fdf3f556ce7acfece698d0b86f9ab086c0158637
Dillon Sellars <dsellars@newrelic.com>
2024-03-15 21:06:27 -0400

    NR-131629 Setup New Relic Deployment markers for lsp-agent - Retry on error (got a ECONNRESET)

--- build/track.js
+++ build/track.js
@@ -5,6 +5,7 @@ const version = process.env.TCBUILD_ASSET_FULL_NAME;
 const entityGuid = process.argv[2];
 const NR_API_KEY = process.env.NR_API_KEY;
 const ERR_EXIT_CODE = 0; // For now non-fatal exit code
+const MAX_RETRIES = 3; // Set the maximum number of retries
 
 if (!version || !entityGuid || !NR_API_KEY) {
   console.error('Missing arguments or environment variables');
@@ -33,21 +34,39 @@ const options = {
   }
 };
 
-const req = https.request(options, res => {
-  let data = '';
+function sendRequest(retries) {
+  const req = https.request(options, res => {
+    let data = '';
 
-  res.on('data', chunk => {
-    data += chunk;
+    res.on('data', chunk => {
+      data += chunk;
+    });
+
+    res.on('end', () => {
+      if (res.statusCode < 200 || res.statusCode >= 400) {
+        console.error('Error sending deployment event to New Relic');
+        console.error(`HTTP Status: ${res.statusCode}`);
+        console.error(`Response: ${data}`);
+        process.exit(ERR_EXIT_CODE);
+      } else {
+        console.log('Deployment event sent to New Relic');
+      }
+    });
   });
 
-  res.on('end', () => {
-    if (res.statusCode < 200 || res.statusCode >= 400) {
-      console.error('Error sending deployment event to New Relic');
-      console.error(`HTTP Status: ${res.statusCode}`);
-      console.error(`Response: ${data}`);
-      process.exit(ERR_EXIT_CODE);
+  req.on('error', error => {
+    if (retries > 0) {
+      console.log('Retrying request...');
+      sendRequest(retries - 1);
     } else {
-      console.log('Deployment event sent to New Relic');
+      console.error('Error sending deployment event to New Relic');
+      console.error(`Response: ${error.message}`);
+      process.exit(ERR_EXIT_CODE);
     }
   });
-});
+
+  req.write(DATA);
+  req.end();
+}
+
+sendRequest(MAX_RETRIES);
