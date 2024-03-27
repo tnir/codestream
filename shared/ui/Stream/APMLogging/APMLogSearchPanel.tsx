936eb8c0f2eed6b4576fadaca353438aac64477d
Eric Jones <etjones319@gmail.com>
2024-03-27 13:14:02 -0700

    Revert developer hardcoded account id

--- shared/ui/Stream/APMLogging/APMLogSearchPanel.tsx
+++ shared/ui/Stream/APMLogging/APMLogSearchPanel.tsx
@@ -482,7 +482,7 @@ export const APMLogSearchPanel = (props: {
 	const fetchPartitions = async (entityAccount: EntityAccount) => {
 		try {
 			const response = await HostApi.instance.send(GetLoggingPartitionsRequestType, {
-				accountId: 1,
+				accountId: entityAccount.accountId,
 			});
 
 			if (!response) {
